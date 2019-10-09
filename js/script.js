9419e5eaa586f5af8be409db9e005ea87666f8ac
isabellawhitfield <270026136@bun2-206-08.acg.ac.nz>
2019-10-09 15:18:33 +1300

    js values

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ document.write() produces on the browser window
 document.write('Greetings from document.write()');
 
  console.log('Hi, I am happy to help you with debugging');
+ console.log('you can\'t delete me');
+
+ //not a secured way
+document.getElementById('heading').innerHTML += "</br>Learning JS , produced by innerHTML</br>";
+
+//secured but reduced performance
+document.getElementById('heading').innerText += 'Hi, I am produced by innerText';
+
+//secured and increased performance
+document.getElementById('heading').textContent += 'Hello from textContent'

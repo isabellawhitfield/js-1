743c60019710eddb639cca6f2ac086e22354f595
isabellawhitfield <270026136@bun2-206-08.acg.ac.nz>
2019-10-09 13:39:30 +1300

    c

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//alert ('Hello from external JS file');
+
+/* multiple lines comment
+Alert() produces output in an alert box
+document.write() produces on the browser window
+*/
+
+document.write('Greetings from document.write()');
+
+ console.log('Hi, I am happy to help you with debugging');
